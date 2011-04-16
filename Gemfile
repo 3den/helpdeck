1e94359bd586e1b8247e714ec56d6fb8bbf60eed
Marcelo Eden <edendroid@gmail.com>
2011-04-16 20:19:56 -0300

    Jammit

--- Gemfile
+++ Gemfile
@@ -2,12 +2,11 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.5'
 #gem 'rails', :git => 'git://github.com/rails/rails.git'
-
+gem 'jammit'
+gem 'yui-compressor'
 gem 'omniauth'
 gem 'twitter'
 
 group :development, :test do
   gem 'sqlite3-ruby', :require => 'sqlite3'
-  gem 'webrat'
-  gem 'ruby-debug'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,9 @@ Helpdeck::Application.configure do
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
+  # Jammit
+  config.gem "jammit"
+
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_view.debug_rjs             = true
